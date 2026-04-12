2b3407de221a19806741f0b71287f475945a4cf1
faizan23804 <riazfaizan237@gmail.com>
2026-04-12 17:29:41 +0530

    update

--- test_env.py
+++ test_env.py
@@ -1,6 +1,9 @@
-# test_env.py
 import os
 from dotenv import load_dotenv # type: ignore
+import pandas as pd
+
+df = pd.read_csv(r'D:\End-to-end-ML\AnomalyDetection\Artifacts\06_04_2026_23_34_30\data_ingestion\feature_store\taxiNYC.csv')
+print(df.head(10))
 
 load_dotenv()  
 
